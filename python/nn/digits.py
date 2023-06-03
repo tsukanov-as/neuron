b9ac061b912231e423f33d661ce8ef479799ab0a
tsukanov-as <tsukanov.as@yandex.ru>
2023-06-04 00:08:58 +0300

    make linter happy

--- python/nn/digits.py
+++ python/nn/digits.py
@@ -2,7 +2,7 @@ from sklearn.datasets import load_digits
 digits = load_digits()
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.05, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.05, random_state=42) # type: ignore
 
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB, ComplementNB, GaussianNB
 clf = MultinomialNB()
